f66ae7d70cc70895c62542e571326e5508bc6af0
Zhen @SHMC328-ZL <zhen.li.zl242@yale.edu>
2014-06-04 16:32:11 -0400

    RDAVIDWebService edited

--- R/RDAVIDWebService_script.R
+++ R/RDAVIDWebService_script.R
@@ -1,21 +1,18 @@
-##-----Use RDAVIDWebservice to access DAVID
-
-data <- neonatal.module.1                                         ##--Read a dataset
+##-----Use RDAVIDWebservice to access DAVID                                   
 
 library("RDAVIDWebService")                                       ##--Load RDAVIDWebService 
 user <- DAVIDWebService$new(email = "zhen.li.zl242@yale.edu")     ##--Log on to DAVID with email
 
-getIdTypes(user) ##--Show gene id types
+getIdTypes(user)                                                  ##--Show gene id types
+getAllAnnotationCategoryNames(user)                               ##--Show annotation categories
 
 ##--Get a list of genes, pay attention to idType
-result <- addList(user, data, 
-                  idType="ENSEMBL_GENE_ID", 
-                  listType="Gene", 
-                  listName = "neonatal.module.1")          
+for (i in 2:40){
+  data <- read.csv(paste("neonatal.module",i, ".csv"), header = T)[, 2] ##--Read a dataset
+  setAnnotationCategories(user, "GOTERM_BP_FAT")                   ##--check a specific category
+  getFunctionalAnnotationChartFile(user, "neonatal_GOterms.txt")#, paste("neonatal.module.",i,"_KEGG.csv"))
+}
+                  
+                  
+show(user)                                                        ##--Information about the run (or just use user)
 
-show(user)                                                         ##--Information about the run (or just use user)
-
-getAllAnnotationCategoryNames(user)                                ##--Show annotation categories
-
-setAnnotationCategories(user, "KEGG_PATHWAY")                      ##--check a specific category
-geneCluster <- getClusterReport(user, type = "Gene") 
