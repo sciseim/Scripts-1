96508fca93cf3e84287d41cccea70aeffe0b294a
Zhen@Home <zhen.li.zl242@yale.edu>
2014-11-16 13:19:31 -0500

    Working Parsing_html.py for hospital-data.com

--- python/Parsing_html.py
+++ python/Parsing_html.py
@@ -7,15 +7,30 @@ Created on Thu Nov 13 15:06:48 2014
 
 from bs4 import BeautifulSoup
 import requests
-import sys   
+import sys
+import time
+import os   
 
-state = sys.argv[1]
-state = "CT"
+
+if len(sys.argv) != 2:
+    print "Please specify a state using abbreviation"
+    sys.exit()
+
+
+state = sys.argv[1] 
+if len(state) != 2:
+    print "Please use abbreviation of a state"
+    print "Usage:"
+    print "      python Parsing_html.py STATE_ABBREVIATION"
+    sys.exit()
+    
+#state = "CT"
 outfile = "%s_nursing_homes.csv"%(state) #Name of output file
 outf = open(outfile, "w")
 outf.write('Name,Number of Beds,Number of Residents,City,State,Contact\n')
-    
-
+outf.close()
+print "File saved to %s"%os.getcwd()
+print "---------------------------------------"
 #----Cook the soup
 url = "http://www.hospital-data.com/dirs/%s-Nursing-Homes.html"%(state)
 page = requests.get(url)
@@ -40,39 +55,58 @@ url_n = []
 for n in nursing_homes:            
     url_n.append("http://www.hospital-data.com/" + n.lstrip("../../"))
 
+outf = open(outfile, "a")
 for n in url_n:
     page = requests.get(n)
+    #page = requests.get("http://www.hospital-data.com/hospitals/DINAN-MEMORIAL-CENTER-TEDESCO-ANNEX-BRIDGEPORT.html")
     soup = BeautifulSoup(page.content)
-    name = soup.find_all('h1', {"align": "center"})
-    
-    for n in name:
-        add = n.contents[0].split("-")
-        hos = add[0]
-        city = add[1].split(",")[0]
-        state = add[1].split(",")[1]
+    name = soup.find('h1', {"align": "center"})
+    print "Retrieve information for %s"%name.text
+    add = name.contents[0].split(" - ")
+    hos = add[0].replace(',', '')
+    city = add[1].split(",")[0]
 
-    if soup.find("Number of Certified Beds:") == True:        
-        info = soup.find_all('div', {"style": "float:left;padding-right:2.5em;"})
-        for item in info:
-            try:
-                phone = item.contents[4].text
-            except:
-                phone = " "
-            try:
-                beds = item.contents[8].text
-            except:
-                beds = " "
-            try:
-                residents = item.contents[11].text
-            except:
-                residents = " "  
-        outf.write('%s,%s,%s,%s,%s,%s\n' %(hos, 
+    if "Number of Certified Beds:" in soup.text:        
+        info = soup.find('div', {"style": "float:left;padding-right:2.5em;"})
+        try:
+            phone = info.contents[4].text
+        except:
+            phone = " "
+        try:
+            beds = info.contents[8].text
+        except:
+            beds = " "
+        try:
+            residents = info.contents[11].text
+        except:
+            residents = " "      
+    elif "Number Of Beds" in soup.text:
+        info = soup.find('div', {"style": "float:left;padding-right:2.5em;"})
+        try:        
+            phone = info.contents[4].text
+        except:
+            phone = " "
+        info = soup.find('div', {"class": "fdivpos"})
+        info = info.find_next('div', {"class": "fdivpos"})
+        try:
+            beds = info.contents[5].text
+        except:
+            beds = " "
+        residents = " "
+    
+    
+    outf.write('%s,%s,%s,%s,%s,%s\n' %(hos, 
                                        beds, 
                                        residents, 
                                        city,
                                        state,
                                        phone))
-    #if  
+    print "Finished retrieving information for %s"%name.text
+    print "---------------------------------------"
+    time.sleep(5)
+
+outf.close() #------close file
+print "All information successfully retrieved for nursing_homes in %s"%state
+    
         
-#-----close file
-outf.close()
+
